42882a1d94c74994e2700803c05a6e8babfee222
o_porpino <dev@oporpino.com>
2025-05-26 15:24:38 -0300

    refactor: rename simple project to avoid issue with namespace

--- SharpConsole.Simple/Infrastructure/User.cs
+++
@@ -1,8 +0,0 @@
-namespace SharpConsole.Example.Infrastructure;
-
-public class User
-{
-  public int Id { get; set; }
-  public string Name { get; set; } = string.Empty;
-  public int Age { get; set; }
-}
